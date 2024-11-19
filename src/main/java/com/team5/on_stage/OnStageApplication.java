c0f6164b12078dea4fb3776fb66f162d93233e3f
Sur0n <skyzephyr333@gmail.com>
2024-11-19 17:38:36 +0900

    settings : init java/spring base files

---
+++ src/main/java/com/team5/on_stage/OnStageApplication.java
@@ -0,0 +1,13 @@
+package com.team5.on_stage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnStageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnStageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team5/on_stage/OnStageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5.on_stage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnStageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
