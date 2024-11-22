528bd397eeb4933699ea3f4a6eb4b6629eb82edb
thoon <winwin_th@naver.com>
2024-11-22 11:10:09 +0900

    fix: Repository 어노테이션 추가

--- src/main/java/com/team5/on_stage/user/repository/UserRepository.java
+++ src/main/java/com/team5/on_stage/user/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.team5.on_stage.user.repository;
 
 import com.team5.on_stage.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     // 이미 생성된 사용자인지, 최초 가입 사용자인지 판단
