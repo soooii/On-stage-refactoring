16eb3572ceeeb8750eaea22a41df6935b06e3906
thoon <winwin_th@naver.com>
2024-11-22 11:20:35 +0900

    feat: SecurityConfig, CorsConfig 추가

--- src/main/java/com/team5/on_stage/global/config/SecurityConfig.java
+++ src/main/java/com/team5/on_stage/global/config/SecurityConfig.java
@@ -1,14 +1,18 @@
 package com.team5.on_stage.global.config;
 
 import com.team5.on_stage.global.config.auth.CustomOAuth2UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Configuration
@@ -26,6 +30,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors((cors) -> cors.configurationSource(new CorsConfig()));
+
+        http
         .csrf((auth) -> auth.disable())
         .formLogin((auth) -> auth.disable())
         .httpBasic((auth) -> auth.disable());
@@ -36,9 +43,9 @@ public class SecurityConfig {
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() /* Swagger */
                 .anyRequest().authenticated());
 
-        http.oauth2Login((oauth2) -> oauth2
-                .userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig
-                        .userService(customOAuth2UserService))));
+//        http.oauth2Login((oauth2) -> oauth2
+//                .userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig
+//                        .userService(customOAuth2UserService))));
 
 
         return http.build();
