a38df110406e40ca4f7268b2e73223061b082548
thoon <winwin_th@naver.com>
2024-11-25 16:55:17 +0900

    fix: 실행 시 오류 발생으로 주석 처리

--- src/main/java/com/team5/on_stage/global/config/jwt/JwtFilter.java
+++ src/main/java/com/team5/on_stage/global/config/jwt/JwtFilter.java
@@ -1,43 +1,43 @@
-package com.team5.on_stage.global.config.jwt;
-
-import com.team5.on_stage.global.config.auth.AuthTokenImpl;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-import java.util.Optional;
-
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class JwtFilter extends OncePerRequestFilter {
-    private final JwtProviderImpl tokenProvider;
-    private final JwtTokenResolver tokenResolver;
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        Optional<String> token = tokenResolver.resolveToken(request);
-
-        if (token.isPresent()) {
-            AuthTokenImpl jwtToken = tokenProvider.convertAuthToken(token.get().split(" ")[1]);
-            String jti = jwtToken.getDate().getId();
-
-//            if (!blacklistService.isBlacklisted(jti) && jwtToken.validate()) {
-//                Authentication authentication = tokenProvider.getAuthentication(jwtToken);
-//                SecurityContextHolder.getContext().setAuthentication(authentication);
-//            }
-
-        }
-        filterChain.doFilter(request, response);
-    }
-
-
-}
+//package com.team5.on_stage.global.config.jwt;
+//
+//import com.team5.on_stage.global.config.auth.AuthTokenImpl;
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import java.io.IOException;
+//import java.util.Optional;
+//
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
+//public class JwtFilter extends OncePerRequestFilter {
+//    private final JwtProviderImpl tokenProvider;
+//    private final JwtTokenResolver tokenResolver;
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+//        Optional<String> token = tokenResolver.resolveToken(request);
+//
+//        if (token.isPresent()) {
+//            AuthTokenImpl jwtToken = tokenProvider.convertAuthToken(token.get().split(" ")[1]);
+//            String jti = jwtToken.getDate().getId();
+//
+////            if (!blacklistService.isBlacklisted(jti) && jwtToken.validate()) {
+////                Authentication authentication = tokenProvider.getAuthentication(jwtToken);
+////                SecurityContextHolder.getContext().setAuthentication(authentication);
+////            }
+//
+//        }
+//        filterChain.doFilter(request, response);
+//    }
+//
+//
+//}
