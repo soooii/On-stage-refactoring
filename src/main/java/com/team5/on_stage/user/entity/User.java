03a0b6fb05d431753a03b931c23ecf003da2a921
thoon <winwin_th@naver.com>
2024-11-25 15:33:19 +0900

    style: 설명 주석 추가

--- src/main/java/com/team5/on_stage/user/entity/User.java
+++ src/main/java/com/team5/on_stage/user/entity/User.java
@@ -19,6 +19,7 @@ public class User {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    // 서비스에서 사용할 이름
     @NotNull
     @Column(name = "nickname", nullable = false)
     private String nickname;
@@ -26,26 +27,30 @@ public class User {
     @Column(name = "description")
     private String description;
 
+    // 소셜 로그인 정보에서 가져온 이메일
+    // Todo: 이메일이 없을 수도 있다. (ex: 깃허브)
+    @NotNull
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    // 소셜 로그인 정보에서 가져온 이름
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    // 소셜 로그인 도메인 + 소셜 로그인 ID 문자열
+    @NotNull
+    @Column(name = "username", nullable = false)
+    private String username;
+
     // Todo: Enum으로 관리할지.
     @Column(name = "verified", nullable = false)
     private Boolean verified;
 
     @NotNull
-    @Column(name = "email", nullable = false, unique = true)
-    private String email;
-
-    @NotNull
     @Enumerated(EnumType.STRING)
     @Column(name = "email_domain", nullable = false)
     private EmailDomain emailDomain;
-
-    @NotNull
-    @Column(name = "name", nullable = false)
-    private String name;
-
-    @NotNull
-    @Column(name = "username", nullable = false)
-    private String username;
 
     @NotNull
     @Enumerated(EnumType.STRING)
