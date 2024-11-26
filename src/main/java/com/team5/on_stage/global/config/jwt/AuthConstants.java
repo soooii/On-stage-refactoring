258f7753b8bce972fd3f6eacea4f33bdb33c9a2a
thoon <131009655+hthoon@users.noreply.github.com>
2024-11-26 16:24:34 +0900

    refactor: 인터페이스로 변경

--- src/main/java/com/team5/on_stage/global/config/jwt/AuthConstants.java
+++ src/main/java/com/team5/on_stage/global/config/jwt/AuthConstants.java
@@ -1,21 +1,22 @@
 package com.team5.on_stage.global.config.jwt;
 
-public class AuthConstants {
+public interface AuthConstants {
 
-    public static final String FRONT_DOMAIN = "http://localhost:3000";
-    public static final String BACK_DOMAIN = "http://localhost:8080";
+    // Domain
+    String FRONT_DOMAIN = "http://localhost:3000";
+    String BACK_DOMAIN = "http://localhost:8080";
 
     // Token Type
-    public static final String REFRESH_TOKEN = "refresh";
-    public static final String ACCESS_TOKEN = "access";
+    String REFRESH_TOKEN = "refresh";
+    String ACCESS_TOKEN = "access";
 
     // Access Token을 전달할 헤더
-    public static final String AUTH_HEADER = "Authorization";
+    String AUTH_HEADER = "Authorization";
 
     // Access Token의 타입
-    public static final String AUTH_TYPE = "Bearer ";
+    String AUTH_TYPE = "Bearer ";
 
     // 각 Token의 유효 시간
-    public static final Long REFRESH_TOKEN_EXPIRED_MS = 600000L;
-    public static final Long ACCESS_TOKEN_EXPIRED_MS = 86400000L;
+    Long REFRESH_TOKEN_EXPIRED_MS = 600000L;
+    Long ACCESS_TOKEN_EXPIRED_MS = 86400000L;
 }
