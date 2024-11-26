fa4a0885c7d53d34f6bd4e1d6d60d0fd92aa1169
thoon <winwin_th@naver.com>
2024-11-26 11:59:15 +0900

    feat: 값을 상수로 관리할 AuthConstants 클래스 생성

---
+++ src/main/java/com/team5/on_stage/global/config/jwt/AuthConstants.java
@@ -0,0 +1,17 @@
+package com.team5.on_stage.global.config.jwt;
+
+public class AuthConstants {
+
+    public static final String FRONT_DOMAIN = "http://localhost:3000";
+    public static final String BACK_DOMAIN = "http://localhost:8080";
+
+    // Access Token을 전달할 헤더
+    public static final String AUTH_HEADER = "Authorization";
+
+    // Access Token의 타입
+    public static final String AUTH_TYPE = "Bearer ";
+
+    // 각 Token의 유효 시간
+    public static final Long REFRESH_TOKEN_EXPIRED_MS = 600000L;
+    public static final Long ACCESS_TOKEN_EXPIRED_MS = 86400000L;
+}
