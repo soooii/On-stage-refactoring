7a95e6f516f5b4f48eda64a2ac210dc3e183100b
HVLOSHIN <halogod9985@naver.com>
2024-11-27 20:11:46 +0900

    오피스아워 커밋

--- src/main/java/com/team5/on_stage/link/controller/LinkController.java
+++ src/main/java/com/team5/on_stage/link/controller/LinkController.java
@@ -1,5 +1,6 @@
 package com.team5.on_stage.link.controller;
 
+import com.team5.on_stage.global.config.jwt.JwtUtil;
 import com.team5.on_stage.link.dto.LinkDTO;
 import com.team5.on_stage.link.dto.LinkResponseDTO;
 import com.team5.on_stage.link.service.LinkService;
@@ -15,6 +16,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/link")
 public class LinkController {
     private final LinkService linkService;
+//    private final JwtUtil jwtUtil;
+//
+//    @GetMapping
+//    public ResponseEntity<LinkResponseDTO> getLink(@RequestHeader("Authorization") String authorizationHeader) {
+//        String username = jwtUtil.getUsername(authorizationHeader.substring(7));
+//        return ResponseEntity.status(HttpStatus.OK).body(linkService.getLink(username));
+//    }
 
     @GetMapping
     public ResponseEntity<LinkResponseDTO> getLink() {
