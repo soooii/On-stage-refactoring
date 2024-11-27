2d4d35cd3129b5814ce3d9585b4d9012910eb2f7
thoon <winwin_th@naver.com>
2024-11-27 17:03:31 +0900

    feat, bug: JwtFilter 추가, LogoutFilter는 수정 필요

--- src/main/java/com/team5/on_stage/global/config/SecurityConfig.java
+++ src/main/java/com/team5/on_stage/global/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import com.team5.on_stage.global.config.auth.CustomClientRegistrationRepo;
 import com.team5.on_stage.global.config.auth.CustomOAuth2AuthorizedClientService;
 import com.team5.on_stage.global.config.auth.CustomOAuth2UserService;
 import com.team5.on_stage.global.config.auth.OAuth2SuccessHandler;
+import com.team5.on_stage.global.config.jwt.CustomLogoutFilter;
+import com.team5.on_stage.global.config.jwt.JwtFilter;
+import com.team5.on_stage.global.config.jwt.JwtUtil;
+import com.team5.on_stage.user.repository.RefreshRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +15,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @RequiredArgsConstructor
 @Configuration
@@ -23,6 +29,8 @@ public class SecurityConfig {
     private final CustomOAuth2AuthorizedClientService customOAuth2AuthorizedClientService;
     private final JdbcTemplate jdbcTemplate;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final JwtUtil jwtUtil;
+    private final RefreshRepository refreshRepository;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -37,15 +45,21 @@ public class SecurityConfig {
                 .cors((cors) -> cors.configurationSource(new CorsConfig()));
 
         http
-        .csrf((auth) -> auth.disable())
-        .formLogin((auth) -> auth.disable())
-        .httpBasic((auth) -> auth.disable());
+                .csrf((auth) -> auth.disable())
+                .formLogin((auth) -> auth.disable())
+                .httpBasic((auth) -> auth.disable())
+                .logout(logout -> logout.logoutUrl("/logout"));
 
         http.authorizeHttpRequests((auth) -> auth
                 .requestMatchers("/").permitAll()
                 .requestMatchers("/api/**").permitAll()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() /* Swagger */
+                .requestMatchers("/usertest").authenticated()
                 .anyRequest().permitAll());
+
+        http
+                .addFilterBefore(new JwtFilter(jwtUtil), OAuth2LoginAuthenticationFilter.class)
+                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), LogoutFilter.class);
 
         http.oauth2Login((oauth2) -> oauth2
                 //.loginPage("/login")
