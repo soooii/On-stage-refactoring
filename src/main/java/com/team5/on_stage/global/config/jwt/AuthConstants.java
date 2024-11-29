2dba1b5e2dd01f65f1757d7bf4e8b4a235269b28
thoon <winwin_th@naver.com>
2024-11-29 13:04:49 +0900

    refactor: 클래스로 변경

--- src/main/java/com/team5/on_stage/global/config/jwt/AuthConstants.java
+++ src/main/java/com/team5/on_stage/global/config/jwt/AuthConstants.java
@@ -1,22 +1,22 @@
 package com.team5.on_stage.global.config.jwt;
 
-public interface AuthConstants {
+public class AuthConstants {
 
     // Domain
-    static String FRONT_DOMAIN = "http://localhost:3000";
-    static String BACK_DOMAIN = "http://localhost:8080";
+    static public String FRONT_DOMAIN = "http://localhost:3000";
+    static public String BACK_DOMAIN = "http://localhost:8080";
 
     // Token Type
-    static String TYPE_REFRESH = "refresh";
-    static String TYPE_ACCESS = "access";
+    static public String TYPE_REFRESH = "refresh";
+    static public String TYPE_ACCESS = "access";
 
     // Access Token을 전달할 헤더
-    static String AUTH_HEADER = "Authorization";
+    static public String AUTH_HEADER = "Authorization";
 
     // Access Token의 타입
-    static String AUTH_TYPE = "Bearer ";
+    static public String AUTH_TYPE = "Bearer ";
 
     // 각 Token의 유효 시간
-    static Long REFRESH_TOKEN_EXPIRED_MS = 86400000L;
-    static Long ACCESS_TOKEN_EXPIRED_MS = 86400000L;
+    static public Long REFRESH_TOKEN_EXPIRED_MS = 86400000L;
+    static public Long ACCESS_TOKEN_EXPIRED_MS = 86400000L;
 }
