ef555a2a10bb4fd5a80a6d13626c503feec68d73
[cloud4] 안송희 <songhee9327@gmail.com>
2024-11-30 23:30:58 +0900

    refactor : querydsl 사용 메서드 추가

--- src/main/java/com/team5/on_stage/summary/repository/SummaryQueryDslRepositoryImpl.java
+++ src/main/java/com/team5/on_stage/summary/repository/SummaryQueryDslRepositoryImpl.java
@@ -1,9 +1,13 @@
 package com.team5.on_stage.summary.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.team5.on_stage.summary.entity.Summary;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static com.team5.on_stage.summary.entity.QSummary.summary1;
 
@@ -23,4 +27,22 @@ public class SummaryQueryDslRepositoryImpl implements SummaryQueryDslRepository 
                 .execute();
     }
 
+    @Override
+    public List<Summary> getSummaryByUserId(Long userId, Pageable pageable) {
+        return queryFactory
+                .selectFrom(summary1)
+                .where(summary1.user.id.eq(userId))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    @Override
+    public long countSummaryByUserId(Long userId) {
+        return queryFactory
+                .select(summary1.count())
+                .from(summary1)
+                .where(summary1.user.id.eq(userId))
+                .fetchOne();
+    }
 }
