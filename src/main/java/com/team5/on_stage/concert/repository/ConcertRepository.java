0b2d0888601f1b9268ddaa37ff5d4829f514ad2e
Sur0n <skyzephyr333@gmail.com>
2024-12-02 01:26:20 +0900

    remove : java files

--- src/main/java/com/team5/on_stage/concert/repository/ConcertRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team5.on_stage.concert.repository;
-
-import com.team5.on_stage.concert.entity.Concert;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConcertRepository extends JpaRepository<Concert,Long> {
-}
