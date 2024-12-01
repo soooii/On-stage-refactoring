e0c3fc271af2f3129e3e6dd05eed344dac5166fe
thoon <winwin_th@naver.com>
2024-12-01 17:28:45 +0900

    refactor: update 기능 분리, username을 파라미터로 사용하도록 변경

--- src/main/java/com/team5/on_stage/user/repository/UserRepository.java
+++ src/main/java/com/team5/on_stage/user/repository/UserRepository.java
@@ -9,14 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    // 이미 생성된 사용자인지, 최초 가입 사용자인지 판단
-    // Todo: 확인을 username으로 하는 게 좋을 것 같다.
-    Boolean existsByEmail(String email);
     User findByUsername(String username);
 
-
-    Optional<User> findByEmail(String email);
-
-
-    Boolean deleteUserByEmail(String email);
+    Boolean deleteUserByUsername(String username);
 }
--- src/main/java/com/team5/on_stage/user/service/UserService.java
+++ src/main/java/com/team5/on_stage/user/service/UserService.java
@@ -1,23 +1,10 @@
 package com.team5.on_stage.user.service;
 
-import com.team5.on_stage.global.config.auth.dto.CustomOAuth2User;
 import com.team5.on_stage.global.constants.ErrorCode;
 import com.team5.on_stage.global.exception.GlobalException;
-import com.team5.on_stage.socialLink.entity.SocialLink;
-import com.team5.on_stage.socialLink.repository.SocialLinkRepository;
-import com.team5.on_stage.theme.entity.Theme;
-import com.team5.on_stage.theme.repository.ThemeRepository;
-//import com.team5.on_stage.user.dto.SignUpDto;
-//import com.team5.on_stage.user.dto.SignUpUserDto;
-import com.team5.on_stage.user.dto.UpdateUserDto;
 import com.team5.on_stage.user.entity.*;
-import com.team5.on_stage.user.repository.TempUserRepository;
 import com.team5.on_stage.user.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -25,104 +12,52 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    private final TempUserRepository tempUserRepository;
-    private final ThemeRepository themeRepository;
-    private final SocialLinkRepository socialLinkRepository;
 
 
-//    public Boolean signUp(SignUpDto signUpDto) {
-//
-////      1. 중복 이메일 검증
-//        if (userRepository.existsByEmail(signUpDto.getEmail())) {
-//            throw new GlobalException(ErrorCode.EMAIL_DUPLICATED);
-//        }
-//
-//        String encodedPassword = bCryptPasswordEncoder.encode(signUpDto.getPassword());
-//
-//        signUpDto.setPassword(encodedPassword);
-//
-//        User user = User.builder()
-//                .nickname(signUpDto.getNickname())
-//                .description(signUpDto.getDescription())
-//                .email(signUpDto.getEmail())
-//                .OAuth2Domain(OAuth2Domain.valueOf(extractDomain(signUpDto.getEmail())))
-//                .build();
-//
-//        userRepository.save(user);
-//
-//        return true;
-//    }
+    public Boolean updateUserNickname(String username,
+                                      String nickname) {
 
+        User user = userRepository.findByUsername(username);
 
-//    @Transactional
-//    public Boolean signUpUser(String username,
-//                              SignUpUserDto signUpUserDto) {
-//
-//        if (tempUserRepository.findByUsername(username) == null) {
-//            throw new GlobalException(ErrorCode.USER_NOT_FOUND);
-//        }
-//
-//        TempUser tempUser = tempUserRepository.findByUsername(username);
-//
-//        User user = User.builder()
-//                .nickname(signUpUserDto.getNickname())
-//                .description(signUpUserDto.getDescription())
-//                .email(tempUser.getEmail())
-//                .OAuth2Domain(OAuth2Domain.valueOf(extractDomain(tempUser.getEmail())))
-//                .name(tempUser.getName())
-//                .username(tempUser.getUsername())
-//                .verified(Verified.UNVERIFIED)
-//                .role(Role.ROLE_USER)
-//                .image(null)
-//                .build();
-//
-//        userRepository.save(user);
-//        tempUserRepository.deleteByUsername(username);
-//
-//        Theme theme = Theme.builder()
-//                .username(tempUser.getUsername())
-//                .build();
-//        themeRepository.save(theme);
-//
-//        SocialLink socialLink = SocialLink.builder()
-//                .username(tempUser.getUsername())
-//                .build();
-//        socialLinkRepository.save(socialLink);
-//
-//        return true;
-//    }
+        if (user == null) {
 
-
-    @Transactional
-    public Boolean updateUserInformation(String email,
-                                         UpdateUserDto updateUserDto) {
-
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new GlobalException(ErrorCode.USER_NOT_FOUND));
-
-        // description 수정
-        if (updateUserDto.getDescription() != null) {
-            user.setDescription(updateUserDto.getDescription());
+            throw new GlobalException(ErrorCode.USER_NOT_FOUND);
         }
 
-        // nickname 수정
-        if (updateUserDto.getNickname() != null) {
-            user.setNickname(updateUserDto.getNickname());
-        }
+        user.setNickname(nickname);
+
+        userRepository.save(user);
 
         return true;
     }
 
 
-    public Boolean deleteUser(String email) {
+    public Boolean updateUserDescription(String username,
+                                         String description) {
 
-        userRepository.findByEmail(email)
-                .orElseThrow(() -> new GlobalException(ErrorCode.USER_NOT_FOUND));
+        User user = userRepository.findByUsername(username);
 
-        userRepository.deleteUserByEmail(email);
+        if (user == null) {
+
+            throw new GlobalException(ErrorCode.USER_NOT_FOUND);
+        }
+
+        user.setDescription(description);
+
+        userRepository.save(user);
 
         return true;
+    }
+
+
+    public Boolean deleteUser(String username) {
+
+        if (userRepository.findByUsername(username) == null) {
+
+            throw new GlobalException(ErrorCode.USER_NOT_FOUND);
+        }
+
+        return userRepository.deleteUserByUsername(username);
     }
 
 
