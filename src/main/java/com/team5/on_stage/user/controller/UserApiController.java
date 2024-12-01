0534254c1e9b88740d4d20482a0cbc3a04cbf709
thoon <winwin_th@naver.com>
2024-12-01 15:32:34 +0900

    style: 미사용 컨트롤러 메서드 주석처리

--- src/main/java/com/team5/on_stage/user/controller/UserApiController.java
+++ src/main/java/com/team5/on_stage/user/controller/UserApiController.java
@@ -1,7 +1,7 @@
 package com.team5.on_stage.user.controller;
 
-import com.team5.on_stage.user.dto.SignUpDto;
-import com.team5.on_stage.user.dto.SignUpUserDto;
+//import com.team5.on_stage.user.dto.SignUpDto;
+//import com.team5.on_stage.user.dto.SignUpUserDto;
 import com.team5.on_stage.user.dto.UpdateUserDto;
 import com.team5.on_stage.user.service.UserService;
 import jakarta.validation.Valid;
@@ -18,20 +18,20 @@ public class UserApiController {
 
 
     // Todo: 아이디, 비밀번호 가입 방식의 필요성 재고.
-    @PostMapping
-    public ResponseEntity<Boolean> basicSignUp(@Valid @RequestBody SignUpDto signUpDto) {
-
-        return ResponseEntity.ok(userService.signUp(signUpDto));
-    }
+//    @PostMapping
+//    public ResponseEntity<Boolean> basicSignUp(@Valid @RequestBody SignUpDto signUpDto) {
+//
+//        return ResponseEntity.ok(userService.signUp(signUpDto));
+//    }
 
 
     // TempUser -> User 회원가입
     // Todo: 추후 인증 정보에서 username 뽑아서 파라미터로 전달할 것
-    @PostMapping("/{username}")
-    public ResponseEntity<Boolean> signUp(@Valid @PathVariable("username") String username, SignUpUserDto dto) {
-
-        return ResponseEntity.ok(userService.signUpUser(username, dto));
-    }
+//    @PostMapping("/{username}")
+//    public ResponseEntity<Boolean> signUp(@Valid @PathVariable("username") String username, SignUpUserDto dto) {
+//
+//        return ResponseEntity.ok(userService.signUpUser(username, dto));
+//    }
 
 
     // 유저 정보 수정
--- src/main/java/com/team5/on_stage/user/dto/SignUpUserDto.java
+++ src/main/java/com/team5/on_stage/user/dto/SignUpUserDto.java
@@ -1,17 +1,17 @@
-package com.team5.on_stage.user.dto;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-
-@Data
-public class SignUpUserDto {
-
-    @NotNull
-    private String nickname;
-
-    private String description;
-
-    private String image;
-
-}
+//package com.team5.on_stage.user.dto;
+//
+//import jakarta.validation.constraints.Email;
+//import jakarta.validation.constraints.NotNull;
+//import lombok.Data;
+//
+//@Data
+//public class SignUpUserDto {
+//
+//    @NotNull
+//    private String nickname;
+//
+//    private String description;
+//
+//    private String image;
+//
+//}
