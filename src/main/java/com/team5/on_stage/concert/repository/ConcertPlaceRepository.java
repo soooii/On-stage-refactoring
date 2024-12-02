94b57904885247fe75b049b9de7080f03d2d4d08
Sur0n <skyzephyr333@gmail.com>
2024-12-02 16:16:01 +0900

    refactor : repository pk값 Long값 지정

--- src/main/java/com/team5/on_stage/concert/repository/ConcertPlaceRepository.java
+++ src/main/java/com/team5/on_stage/concert/repository/ConcertPlaceRepository.java
@@ -3,5 +3,6 @@ package com.team5.on_stage.concert.repository;
 import com.team5.on_stage.concert.entity.ConcertPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ConcertPlaceRepository extends JpaRepository<ConcertPlace,String> {
+// pk타입 적기
+public interface ConcertPlaceRepository extends JpaRepository<ConcertPlace,Long> {
 }
--- src/main/java/com/team5/on_stage/concert/repository/RelateRepository.java
+++ src/main/java/com/team5/on_stage/concert/repository/RelateRepository.java
@@ -3,5 +3,5 @@ package com.team5.on_stage.concert.repository;
 import com.team5.on_stage.concert.entity.Relate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RelateRepository extends JpaRepository<Relate, String> {
+public interface RelateRepository extends JpaRepository<Relate,Long> {
 }
