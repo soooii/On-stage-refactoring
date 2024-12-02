9d7eb63d41b100ed493d91fde5a1bb3b31fb6b98
thoon <winwin_th@naver.com>
2024-12-02 19:41:59 +0900

    feat: 임시 닉네임 생성 중복검사 추가 및 StringBuilder 사용

--- src/main/java/com/team5/on_stage/global/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/team5/on_stage/global/config/auth/CustomOAuth2UserService.java
@@ -96,19 +96,23 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     }
 
 
-    // Todo: 중복 검사
+    // 임시 닉네임 생성
     public String getTempNickname() {
+        char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                'U', 'V', 'W', 'X', 'Y', 'Z' };
 
-        char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
-                'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        String tempNickname;
+        do {
+            StringBuilder nicknameBuilder = new StringBuilder();
+            for (int i = 0; i < 10; i++) {
+                int idx = (int) (charSet.length * Math.random());
+                nicknameBuilder.append(charSet[idx]);
+            }
+            tempNickname = nicknameBuilder.toString();
+        } while (userRepository.existsByNickname(tempNickname));
 
-        String tempNickname = "";
-
-        int idx = 0;
-        for (int i = 0; i < 10; i++) {
-            idx = (int) (charSet.length * Math.random());
-            tempNickname += charSet[idx];
-        }
         return tempNickname;
     }
 }
