5db8490c6059dc1ac1f324ecd0591caccf7fe012
[cloud4] 안송희 <songhee9327@gmail.com>
2024-12-03 17:09:39 +0900

    feat : Batch 설정

---
+++ src/main/java/com/team5/on_stage/global/batch/BatchScheduler.java
@@ -0,0 +1,33 @@
+package com.team5.on_stage.global.batch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
+@RequiredArgsConstructor
+@Slf4j
+public class BatchScheduler {
+
+    private final JobLauncher jobLauncher;
+    private final Job saveSummaryJob;
+
+    // 매일 배치 작업 실행 (현재 시간 기준 3개월 지난 summary 새로 저장)
+    @Scheduled(cron = "0 0 0 * * *")
+    public void runSummaryCheck() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        jobLauncher.run(saveSummaryJob, new JobParameters());
+        log.info("runSummaryCheck 실행 완료");
+    }
+
+
+}
