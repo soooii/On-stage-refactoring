38dfaddce49c7151c2c952394c5aff030b586be4
thoon <winwin_th@naver.com>
2024-12-05 10:36:25 +0900

    setting: 엔드포인트 인가 설정 시작

---
+++ src/main/java/com/team5/on_stage/global/config/path/SecurityPath.java
@@ -0,0 +1,34 @@
+package com.team5.on_stage.global.config.path;
+
+public class SecurityPath {
+    public static final String[] ONLY_GET_WHITELIST = {
+            "/",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+
+    };
+    public static final String[] ONLY_POST_WHITELIST = {
+            "/",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+
+    };
+    public static final String[] ONLY_PATCH_WHITELIST = {
+            "/",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+
+    };
+    public static final String[] ONLY_PUT_WHITELIST = {
+            "/",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+
+    };
+    public static final String[] ONLY_DELETE_WHITELIST = {
+            "/",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+
+    };
+}
--- src/main/java/com/team5/on_stage/global/config/path/SecurityUserPath.java
+++ src/main/java/com/team5/on_stage/global/config/path/SecurityUserPath.java
@@ -1,12 +1,26 @@
-package com.team5.on_stage.global.config;
+package com.team5.on_stage.global.config.path;
 
-public class UserSecurityPath {
+public class SecurityUserPath {
+
     public static final String[] ONLY_GET_WHITELIST = {
+
+
     };
     public static final String[] ONLY_POST_WHITELIST = {
+
+
+    };
+    public static final String[] ONLY_PUT_WHITELIST = {
+
+
     };
     public static final String[] ONLY_PATCH_WHITELIST = {
+
+
     };
     public static final String[] ONLY_DELETE_WHITELIST = {
+
+
     };
+
 }
