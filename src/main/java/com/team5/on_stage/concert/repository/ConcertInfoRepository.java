268867da302a7317d2a57d3f555bc6d455782258
Sur0n <skyzephyr333@gmail.com>
2024-12-07 18:48:58 +0900

    refactor : entity 연관관계 검색 기능 추가

--- src/main/java/com/team5/on_stage/concert/repository/ConcertInfoRepository.java
+++ src/main/java/com/team5/on_stage/concert/repository/ConcertInfoRepository.java
@@ -1,10 +1,19 @@
 package com.team5.on_stage.concert.repository;
 
 import com.team5.on_stage.concert.entity.ConcertInfo;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ConcertInfoRepository extends JpaRepository<ConcertInfo,Long> {
+public interface ConcertInfoRepository extends JpaRepository<ConcertInfo, String> {
+    // concert_nm으로 검색
+    @EntityGraph(attributePaths = {
+        "concertDetail.concertPlace",
+                "concertDetail.relate"
+    })
+    List<ConcertInfo> findByPrfnmContaining(String concertNm);
     boolean existsByMt20id(String mt20id); // 유니크 키 확인 메서드
 }
--- src/main/java/com/team5/on_stage/concert/repository/RelateRepository.java
+++ src/main/java/com/team5/on_stage/concert/repository/RelateRepository.java
@@ -4,5 +4,4 @@ import com.team5.on_stage.concert.entity.Relate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RelateRepository extends JpaRepository<Relate,Long> {
-    boolean existsByMt20id(String mt20id); // 유니크 키 확인 메서드
 }
