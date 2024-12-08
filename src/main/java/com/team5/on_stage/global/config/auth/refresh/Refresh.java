0ca0bb28e8ab0652cdd85f922ac53c59a2364877
thoon <winwin_th@naver.com>
2024-12-08 11:03:08 +0900

    refactor: Refresh, 인증 코드 Redis 통합 관리 위해 주석 처리

--- src/main/java/com/team5/on_stage/global/config/auth/refresh/Refresh.java
+++ src/main/java/com/team5/on_stage/global/config/auth/refresh/Refresh.java
@@ -1,36 +1,36 @@
-package com.team5.on_stage.global.config.auth.refresh;
-
-//import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.redis.core.RedisHash;
-import org.springframework.data.redis.core.TimeToLive;
-import org.springframework.data.redis.core.index.Indexed;
-
-@Getter @Setter
-@NoArgsConstructor
-@RedisHash(value = "redis_refresh")
-public class Refresh {
-
-    @Id
-    @Indexed
-    private String refreshToken;
-
-    // 사용자 식별
-    private String username;
-
-    @TimeToLive
-    private Long timeToLive;
-
-    @Builder
-    public Refresh(String username,
-                   String refreshToken) {
-
-        this.username = username;
-        this.refreshToken = refreshToken;
-        this.timeToLive = 1000L * 60 * 60 * 24 * 1;
-    }
-}
+//package com.team5.on_stage.global.config.auth.refresh;
+//
+////import jakarta.persistence.*;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.redis.core.RedisHash;
+//import org.springframework.data.redis.core.TimeToLive;
+//import org.springframework.data.redis.core.index.Indexed;
+//
+//@Getter @Setter
+//@NoArgsConstructor
+//@RedisHash(value = "redis_refresh")
+//public class Refresh {
+//
+//    @Id
+//    @Indexed
+//    private String refreshToken;
+//
+//    // 사용자 식별
+//    private String username;
+//
+//    @TimeToLive
+//    private Long timeToLive;
+//
+//    @Builder
+//    public Refresh(String username,
+//                   String refreshToken) {
+//
+//        this.username = username;
+//        this.refreshToken = refreshToken;
+//        this.timeToLive = 1000L * 60 * 60 * 24 * 1;
+//    }
+//}
--- src/main/java/com/team5/on_stage/global/config/auth/refresh/RefreshRepository.java
+++ src/main/java/com/team5/on_stage/global/config/auth/refresh/RefreshRepository.java
@@ -1,13 +1,13 @@
-package com.team5.on_stage.global.config.auth.refresh;
-
-import org.springframework.data.keyvalue.repository.KeyValueRepository;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface RefreshRepository extends CrudRepository<Refresh, String>, KeyValueRepository<Refresh, String> {
-
-    Optional<Refresh> findByRefreshToken(String refreshToken);
-
-    void deleteByRefreshToken(String refreshToken);
-}
+//package com.team5.on_stage.global.config.auth.refresh;
+//
+//import org.springframework.data.keyvalue.repository.KeyValueRepository;
+//import org.springframework.data.repository.CrudRepository;
+//
+//import java.util.Optional;
+//
+//public interface RefreshRepository extends CrudRepository<Refresh, String>, KeyValueRepository<Refresh, String> {
+//
+//    Optional<Refresh> findByRefreshToken(String refreshToken);
+//
+//    void deleteByRefreshToken(String refreshToken);
+//}
