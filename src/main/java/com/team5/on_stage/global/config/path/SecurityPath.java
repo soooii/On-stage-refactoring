3c408e005e3a2897429c138a166818d90529996b
thoon <winwin_th@naver.com>
2024-12-11 18:03:54 +0900

    setting: User, Analytics 경로 추가

--- src/main/java/com/team5/on_stage/global/config/path/SecurityPath.java
+++ src/main/java/com/team5/on_stage/global/config/path/SecurityPath.java
@@ -1,46 +1,42 @@
 package com.team5.on_stage.global.config.path;
 
 public class SecurityPath {
-    public static final String[] ONLY_GET_WHITELIST = {
+    private static final String[] COMMON_WHITELIST = {
             "/",
             "/swagger-ui/**",
             "/v3/api-docs/**",
-
+    };
+    public static final String[] ONLY_GET_WHITELIST = {
             /* Summary */
             "/api/summary/{username}",
+            /* Analytics */
+            "/api/analytics/dashboard",
+            "/api/analytics/get-ip"
     };
     public static final String[] ONLY_POST_WHITELIST = {
-            "/",
-            "/swagger-ui/**",
-            "/v3/api-docs/**",
-
+            /* User */
+            "/login",
+            "/logout",
+            "/api/auth/reissue",
             /* Summary */
             "/api/summary/{username}",
             /* Article */
             "/api/article/{username}",
+            /* Analytics */
+            "/api/analytics/page",
+            "/api/analytics/link",
+            "/api/analytics/socialLink"
     };
     public static final String[] ONLY_PATCH_WHITELIST = {
-            "/",
-            "/swagger-ui/**",
-            "/v3/api-docs/**"
-
 
     };
     public static final String[] ONLY_PUT_WHITELIST = {
-            "/",
-            "/swagger-ui/**",
-            "/v3/api-docs/**"
 
     };
     public static final String[] ONLY_DELETE_WHITELIST = {
-            "/",
-            "/swagger-ui/**",
-            "/v3/api-docs/**",
-
             /* Summary */
             "/api/summary/{username}",
             /* Article */
-            "/api/article/{username}",
-
+            "/api/article/{username}"
     };
 }
